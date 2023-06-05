61f81c38231d5eae059a53af3897ea671a4c544d
Svenstar74 <sven.firmbach@gmx.de>
2023-06-05 11:26:39 +0200

    Update code

--- screens/stack/LoginScreen/LoginScreen.tsx
+++ screens/stack/LoginScreen/LoginScreen.tsx
@@ -1,109 +1,92 @@
 import { useRef, useState } from 'react';
 import { Dimensions, Image, KeyboardAvoidingView, Pressable, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
-import Recaptcha, { RecaptchaHandles } from "react-native-recaptcha-that-works";
+import Toast from 'react-native-root-toast';
+import { AxiosError } from 'axios';
+import Recaptcha, { RecaptchaHandles } from 'react-native-recaptcha-that-works';
 import { WEB_URL, RECAPTCHA_SITE_KEY } from '@env';
 
 import { useAppDispatch } from '../../../store/hooks';
 import { login } from '../../../store/authSlice';
+import useApiClient from '../../../hooks/useApiClient';
+import useLogger from '../../../hooks/useLogger';
 import PageTitle from '../../../components/PageTitle';
 
 import SimpleWhiteButton from '../../../components/SimpleWhiteButton';
-import useApiClient from '../../../hooks/useApiClient';
 import PasswordResetModal from './PasswordResetModal';
 import Colors from '../../../assets/colors';
-import { AxiosError } from 'axios';
-import Toast from 'react-native-root-toast';
-
 
 function LoginScreen() {
   const apiClient = useApiClient();
+  const logger = useLogger();
   const dispatch = useAppDispatch();
-  
+
   const recaptcha = useRef<RecaptchaHandles>(null);
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  
+
   async function onLogin(token: string) {
+    if (email === '' || password === '') {
+      Toast.show('Please enter your username and password',
+        {
+          duration: Toast.durations.LONG,
+          backgroundColor: '#ED7878',
+          textColor: '#000000',
+          opacity: 1,
+        }
+      );
+      
+      return;
+    }
 
     try {
       const result = await apiClient.postLogin(email, password, token);
-      
+
       if (result !== undefined) {
-        dispatch(login({
-          accessToken: result.access_token,
-          firstName: result.user.first_name,
-          lastName: result.user.last_name,
-          email: result.user.email,
-          userId: result.user.user_uuid,
-          quizId: result.user.quiz_id,
-        }));
+        dispatch(
+          login({
+            accessToken: result.access_token,
+            firstName: result.user.first_name,
+            lastName: result.user.last_name,
+            email: result.user.email,
+            userId: result.user.user_uuid,
+            quizId: result.user.quiz_id,
+          })
+        );
       }
     } catch (error) {
       if (error instanceof AxiosError) {
-        // console.log(`Status: ${error.response?.status}`);
-        // console.log(`Error: ${error.response?.data.error}`);
-
-        // if(error.response?.status === 401){
-        //   if( error.response.data.error === 'Wrong email or password. Try again.'){
-        //     Toast.show('Wrong email or password. Try again.', {
-        //       duration: Toast.durations.LONG,
-        //       backgroundColor: '#BDFADC',
-        //       textColor: '#000000',
-        //       opacity: 1,
-        //     });
-        //   }
-         
-        // }
-        if(error.response?.status === 401){
-          if( error.response.data.error){
-            Toast.show(error.response.data.error, {
-              duration: Toast.durations.LONG,
-              backgroundColor: '#BDFADC',
-              textColor: '#000000',
-              opacity: 1,
-            });
+        Toast.show(
+          error.response?.data.error ?? 'Unexpected Error. Please try again.',
+          {
+            duration: Toast.durations.LONG,
+            backgroundColor: '#ED7878',
+            textColor: '#000000',
+            opacity: 1,
           }
-         
+        );
+
+        if (error.response?.status === 400) {
+          if (error.response?.data.error) {
+            logger.logError('Error while logging in on the LoginScreen.tsx');
+          }
         }
-
-        if(error.response?.status === 400){
-          if(error.response?.data.error){
-            console.log(error.response?.data.error)
-        }
-
+      }
     }
-
-        // if(error.response?.status === 400){
-        //   if( error.response?.data.error === 'Email and password must be included in the request body.'){
-        //     console.log('Email and password must be included in the request body.')
-        //   }else if (error.response?.data.error === 'Email, password and recaptcha must be included in the request body.'){
-        //     console.log('Email, password and recaptcha must be included in the request body.')
-        //   }
-        //   else if (error.response?.data.error === 'Recaptcha token must be included in the request body.'){
-        //     console.log('Recaptcha token must be included in the request body.')
-        //   }
-         
-        // }
-        
-
-    }
-      }    
   }
-  
+
   // Password Reset Request Modal
   const [showModal, setShowModal] = useState(false);
 
   return (
     <ScrollView contentContainerStyle={[styles.container, { backgroundColor: Colors.themeBright }]}>
-      <KeyboardAvoidingView behavior='padding'>
+      <KeyboardAvoidingView behavior="padding">
         <View style={styles.container}>
-          <Image style={styles.image} source={require('../../../assets/cm-logo.png')} />
+          <Image style={styles.image} source={require("../../../assets/cm-logo.png")} />
           <PageTitle>Climate Mind</PageTitle>
-          
+
           <Text style={styles.boldText}>Sign In</Text>
-          
-          
+
           <TextInput
             placeholder="Email"
             keyboardType="email-address"
@@ -123,7 +106,7 @@ function LoginScreen() {
           <View style={{ flexDirection: 'row' }}>
             <Text style={[styles.boldText, { marginRight: 20 }]}>Forgot your password?</Text>
             <Pressable onPress={() => setShowModal(true)}>
-              <Text style={[styles.boldText, { textDecorationLine: 'underline'}]}>Send reset link</Text>
+              <Text style={[styles.boldText, { textDecorationLine: "underline"}]}>Send reset link</Text>
             </Pressable>
           </View>
 
@@ -135,14 +118,13 @@ function LoginScreen() {
             onExpire={() => {}}
             size="normal"
             onError={(err) => {
-              console.log(err)
-              if(err === null)
-                  Toast.show('Captcha did not succeed.', {
-                    duration: Toast.durations.LONG,
-                    backgroundColor: '#BDFADC',
-                    textColor: '#000000',
-                    opacity: 1,
-                  });
+              if (err === null)
+                Toast.show('Captcha did not succeed.', {
+                  duration: Toast.durations.LONG,
+                  backgroundColor: '#ED7878',
+                  textColor: '#000000',
+                  opacity: 1,
+                });
             }}
           />
 
@@ -179,7 +161,7 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     borderTopLeftRadius: 5,
     borderTopRightRadius: 5,
-  }
+  },
 });
 
 export default LoginScreen;
