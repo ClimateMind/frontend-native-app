f14b9481a544ad8883d1dc99bc9b1dc676547360
Svenstar74 <sven.firmbach@gmx.de>
2023-06-08 17:49:05 +0200

    clean up

--- navigation/BottomNavigation.tsx
+++ navigation/BottomNavigation.tsx
@@ -1,9 +1,9 @@
 import { Text } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Entypo } from '@expo/vector-icons';
-import { MaterialIcons } from '@expo/vector-icons';
+
 import { Ionicons } from '@expo/vector-icons';
-import { MaterialCommunityIcons } from '@expo/vector-icons'; 
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { useAppSelector } from '../store/hooks';
 
 // Tab Screens
@@ -24,108 +24,66 @@ export type BottomNavigationParams = {
 const Tabs = createBottomTabNavigator<BottomNavigationParams>();
 
 function BottomNavigation() {
-  const user = useAppSelector(state => state.auth.user);
-  
+  const user = useAppSelector((state) => state.auth.user);
+
   return (
-    <Tabs.Navigator initialRouteName='ClimateFeedStack' screenOptions={{ headerShown: false }}>
+    <Tabs.Navigator initialRouteName="ClimateFeedStack" screenOptions={
+      {
+        headerShown: false,
+        tabBarActiveTintColor: '#07373b',
+        tabBarInactiveTintColor: '#77AAAF',
+        tabBarLabelStyle: {
+          marginBottom: 5,
+          fontFamily: 'nunito-medium',
+        },
+        tabBarStyle: {
+          height: 55,
+        }
+      }
+    }
+    >
       <Tabs.Screen
-        name='ProfileScreen'
+        name="ProfileScreen"
         component={ProfileScreen}
         options={{
           tabBarLabel: 'Profile',
-          tabBarIcon: ({color}) => <Text style={{fontSize: 20, color:color,fontWeight:'bold'}}>{user.firstName[0] + user.lastName[0]}</Text>,
-          tabBarActiveTintColor: '#07373b',
-          tabBarInactiveTintColor: '#77AAAF',
-          tabBarLabelStyle:{
-            marginBottom:5,
-            fontFamily: 'nunito-medium'
-          
-           },
-           tabBarIconStyle:{
-            marginTop:5,
-           },
-           
+          tabBarIcon: ({ color }) => (
+            <Text style={{ fontSize: 20, color, fontWeight: 'bold' }}>
+              {user.firstName[0] + user.lastName[0]}
+            </Text>
+          ),
         }}
-
-        
       />
       <Tabs.Screen
-        name='ClimateFeedStack'
+        name="ClimateFeedStack"
         component={ClimateFeedStack}
         options={{
           tabBarLabel: 'Home',
-          tabBarIcon: ({color}) => <Entypo name="home" size={24} color={color} />,
-          tabBarActiveTintColor: '#07373b',
-          tabBarInactiveTintColor: '#77AAAF',
-          tabBarLabelStyle:{
-            marginBottom:5,
-            fontFamily: 'nunito-medium'
-           
-          
-           },
-           tabBarIconStyle:{
-            marginTop:5,
-           }
-           
+          tabBarIcon: ({ color }) => <Entypo name="home" size={24} color={color} />,
         }}
       />
       <Tabs.Screen
-        name='SolutionsFeedStack'
+        name="SolutionsFeedStack"
         component={SolutionsFeedStack}
         options={{
           tabBarLabel: 'Actions',
-          tabBarIcon: ({color}) => <Ionicons name="bulb-sharp" size={24} color={color}/>,
-          tabBarActiveTintColor: '#07373b',
-          tabBarInactiveTintColor: '#77AAAF',
-          tabBarLabelStyle:{
-            marginBottom:5,
-            fontFamily: 'nunito-medium'
-          
-           },
-           tabBarIconStyle:{
-            marginTop:5,
-           }
-          
-          
+          tabBarIcon: ({ color }) => <Ionicons name="bulb-sharp" size={24} color={color} />,
         }}
       />
       <Tabs.Screen
-        name='MythsFeedStack'
+        name="MythsFeedStack"
         component={MythsFeedStack}
         options={{
           tabBarLabel: 'Myths',
-          tabBarIcon: ({color}) => <MaterialCommunityIcons name="chat-alert" size={24} color={color} />,
-          tabBarActiveTintColor: '#07373b',
-          tabBarInactiveTintColor: '#77AAAF',
-          tabBarLabelStyle:{
-            marginBottom:5,
-            fontFamily: 'nunito-medium'
-          
-           },
-           tabBarIconStyle:{
-            marginTop:5,
-           }
-           
+          tabBarIcon: ({ color }) => <MaterialCommunityIcons name="chat-alert" size={24} color={color} />,
         }}
       />
       <Tabs.Screen
-        name='ConversationsStack'
+        name="ConversationsStack"
         component={ConversationsStack}
         options={{
           tabBarLabel: 'Talk',
-          tabBarIcon: ({color}) => <Entypo name="chat" size={24} color={color} />,
-          tabBarActiveTintColor: '#07373b',
-          tabBarInactiveTintColor: '#77AAAF',
-          tabBarLabelStyle:{
-            marginBottom:5,
-            fontFamily: 'nunito-medium'
-          
-           },
-           tabBarIconStyle:{
-            marginTop:5,
-           }
-         
-           
+          tabBarIcon: ({ color }) => <Entypo name="chat" size={24} color={color} />,
         }}
       />
     </Tabs.Navigator>
