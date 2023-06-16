16a8a69608cb8bd6b8b9a33178348c73379c5000
epixieme <kirstie_hayes@hotmail.com>
2023-06-16 17:48:02 +0100

    code done for modal and copying the link

--- screens/tabs/ConversationsScreen/ConversationsScreen.tsx
+++ screens/tabs/ConversationsScreen/ConversationsScreen.tsx
@@ -1,5 +1,14 @@
 import { useState } from 'react';
-import { Dimensions, Pressable, StyleSheet, Text, TextInput, View } from 'react-native';
+import {
+  Dimensions,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Modal,
+  Alert,
+} from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
 import * as Clipboard from 'expo-clipboard';
 import { WEB_URL } from '@env';
@@ -14,47 +23,137 @@ function ConversationsScreen() {
 
   const [recipient, setRecipient] = useState('');
   const [showConversationsDrawer, setShowConversationsDrawer] = useState(false);
-  
+  const [modalVisible, setModalVisible] = useState(false);
+  const [copiedText, setCopiedText] = useState('');
   async function createLink() {
     if (recipient === '') {
       return;
     }
+  
 
     const result = await apiClient.createConversationInvite(recipient);
     Clipboard.setStringAsync(WEB_URL + '/landing/' + result.conversationId);
+   getLink()
   }
-  
+
+  async function getLink(){
+    const getResult = await Clipboard.getStringAsync();
+    setCopiedText(getResult);
+    setModalVisible(true);
+  }
+
   return (
     <View style={styles.container}>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert('Modal has been closed.');
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+          <Text style={styles.modalText}>Copy Link</Text>
+          <Text style={styles.modalText}>Unique for {recipient}</Text>
+            <Text style={styles.modalText}>{copiedText}</Text>
+            <Pressable onPress={getLink}>
+              <Text>Copy</Text>
+              </Pressable>
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setModalVisible(!modalVisible)}
+            >
+              <Text style={styles.textStyle}>Hide Modal</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
       <Text style={styles.title}>Start a conversation</Text>
       <Text style={styles.mainText}>
-        Create a personalized link for each person you want to talk to. Then share it, so they can
-        take the quiz, discover your shared values, and pick topics to talk about.
+        Create a personalized link for each person you want to talk to. Then
+        share it, so they can take the quiz, discover your shared values, and
+        pick topics to talk about.
       </Text>
-      <Text style={styles.smallText}>We will send you an email when they agree to share their results with you!</Text>
+      <Text style={styles.smallText}>
+        We will send you an email when they agree to share their results with
+        you!
+      </Text>
 
       <Text style={styles.label}>Name of recipient </Text>
       <TextInput
         placeholder='Try "Peter Smith" or "Mom"'
-        autoCapitalize='sentences'
-        
+        autoCapitalize="sentences"
         autoCorrect={false}
         onChangeText={(value) => setRecipient(value)}
         style={styles.input}
       />
-      <SimpleWhiteButton disabled={recipient === ''} text='CREATE LINK' onPress={createLink} />
+      <SimpleWhiteButton
+        disabled={recipient === ''}
+        text="CREATE LINK"
+        onPress={createLink}
+      />
 
-      <Pressable onPress={() => setShowConversationsDrawer(true)} style={styles.openDrawerButton}>
-      <AntDesign name="up" size={24} color="black" />
+      <Pressable
+        onPress={() => setShowConversationsDrawer(true)}
+        style={styles.openDrawerButton}
+      >
+        <AntDesign name="up" size={24} color="black" />
         <Text style={{ fontWeight: 'bold' }}>Ongoing Conversations</Text>
       </Pressable>
 
-      <ConversationsDrawer open={showConversationsDrawer} onClose={() => setShowConversationsDrawer(false)} />
+      <ConversationsDrawer
+        open={showConversationsDrawer}
+        onClose={() => setShowConversationsDrawer(false)}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: '#F194FF',
+  },
+  buttonClose: {
+    backgroundColor: '#2196F3',
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+  },
+  // all elements above are new for te
   container: {
     flex: 1,
     justifyContent: 'center',
