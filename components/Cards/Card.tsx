a155df731b9cc8bdd664519476d5acf710d4393f
epixieme <kirstie_hayes@hotmail.com>
2023-07-28 11:39:20 +0100

    changed shadows for ios

--- components/Cards/Card.tsx
+++ components/Cards/Card.tsx
@@ -1,16 +1,12 @@
-import { StyleSheet, View, ViewStyle } from "react-native";
+import { StyleSheet, View, ViewStyle } from 'react-native';
 
 interface Props {
   children: React.ReactNode;
   style?: ViewStyle | ViewStyle[];
 }
 
-function Card({ children, style={} }: Props) {
-  return (
-    <View style={[styles.card, style]}>
-      { children }
-    </View>
-  );
+function Card({ children, style = {} }: Props) {
+  return <View style={[styles.card, style]}>{children}</View>;
 }
 
 const styles = StyleSheet.create({
@@ -19,6 +15,13 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     width: '100%',
     elevation: 5,
+    shadowColor: '#000000',
+    shadowOffset: {
+      width: 0,
+      height: 5,
+    },
+    shadowOpacity: 0.21,
+    shadowRadius: 6.65,
   },
 });
 
