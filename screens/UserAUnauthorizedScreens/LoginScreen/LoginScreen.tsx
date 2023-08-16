987d8672ffd44a9d120ad1c317f607b472a2d8b8
Svenstar74 <sven.firmbach@gmx.de>
2023-08-16 12:06:55 +0200

    Show recaptcha only after failed login attempts

--- screens/UserAUnauthorizedScreens/LoginScreen/LoginScreen.tsx
+++ screens/UserAUnauthorizedScreens/LoginScreen/LoginScreen.tsx
@@ -23,15 +23,18 @@ function LoginScreen() {
   const dispatch = useAppDispatch();
 
   const recaptcha = useRef<RecaptchaHandles>(null);
+  const [loginAttempts, setLoginAttempts] = useState(0);
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  async function onLogin(token: string) {
+  async function onLogin(token?: string) {
     if (email === '' || password === '') {      
       showErrorToast('Please enter your username and password');
       return;
     }
+
+    setLoginAttempts((current) => current + 1);
 
     try {
       const result = await apiClient.postLogin(email.trim(), password, token);
@@ -99,17 +102,25 @@ function LoginScreen() {
             </Pressable>
           </View>
 
-          <Recaptcha
-            ref={recaptcha}
-            siteKey={RECAPTCHA_SITE_KEY}
-            baseUrl={WEB_URL}
-            onVerify={(token: string) => onLogin(token)}
-            onExpire={() => {}}
-            size="normal"
-            onError={(err) => { err ?? showErrorToast('Captcha did not load.') }}
-          />
+          {loginAttempts >= 4 && (
+            <>
+              <Recaptcha
+                ref={recaptcha}
+                siteKey={RECAPTCHA_SITE_KEY}
+                baseUrl={WEB_URL}
+                onVerify={(token: string) => onLogin(token)}
+                onExpire={() => {}}
+                size="normal"
+                onError={(err) => {
+                  err ?? showErrorToast('Captcha did not load.');
+                }}
+              />
 
-          <SimpleWhiteTextButton style={styles.loginButton} text="LOG IN" onPress={() => recaptcha.current?.open()} />
+              <SimpleWhiteTextButton style={styles.loginButton} text="LOG IN" onPress={() => recaptcha.current?.open()} />
+            </>
+          )}
+
+          {loginAttempts < 4 && <SimpleWhiteTextButton style={styles.loginButton} text="LOG IN" onPress={() => onLogin()} />}
 
           <PasswordResetModal show={showModal} onCancel={() => {setShowModal(false)}} onSubmit={() => setShowModal(false)} />
         </View>
