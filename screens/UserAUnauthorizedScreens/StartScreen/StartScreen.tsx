29ccd20f7354079c4e51d0f3ba14075f156bcc6e
Sven Firmbach <sven.firmbach@gmx.de>
2023-08-22 13:56:50 +0200

    Update StartScreen.tsx

--- screens/UserAUnauthorizedScreens/StartScreen/StartScreen.tsx
+++ screens/UserAUnauthorizedScreens/StartScreen/StartScreen.tsx
@@ -50,7 +50,7 @@ function StartScreen({ navigation }: Props) {
           find solutions they like.
         </BodyText>
         
-        <Pressable delayLongPress={2000} onLongPress={() => showSuccessToast("Test5: " + TEST_VALUE)}>
+        <Pressable delayLongPress={2000} onLongPress={() => showSuccessToast("Test6: " + TEST_VALUE)}>
           <Image source={require('../../../assets/ConnectTheDots.png')} />
         </Pressable>
       </View>
