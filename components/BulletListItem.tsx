b3dfe1b4cd1c673802df45df8442c36cd4549946
Svenstar74 <sven.firmbach@gmx.de>
2023-09-03 21:52:46 +0200

    change copy and fix cutoff text

--- components/BulletListItem.tsx
+++ components/BulletListItem.tsx
@@ -1,17 +1,21 @@
-import { StyleProp, StyleSheet, View, ViewStyle } from "react-native";
+import { StyleProp, StyleSheet, View, ViewStyle } from 'react-native';
 import { Entypo } from '@expo/vector-icons';
-import BodyText from "./TextStyles/BodyText";
+import BodyText from './TextStyles/BodyText';
 
 interface Props {
   children: string;
   textStyle?: StyleProp<ViewStyle>;
 }
 
-function BulletListItem({ children, textStyle={} }: Props) {
+function BulletListItem({ children, textStyle = {} }: Props) {
   return (
     <View style={styles.container}>
       <Entypo name="dot-single" size={24} color="black" />
-      <BodyText style={textStyle}>{children}</BodyText>
+
+      {/* This View with flex: 1 is a fix. Otherwise the text might be cutoff at the end of the sentence. */}
+      <View style={{ flex: 1 }}>
+        <BodyText style={textStyle}>{children}</BodyText>
+      </View>
     </View>
   );
 }
