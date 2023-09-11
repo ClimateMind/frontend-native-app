87bbb4fd9109639c62776714c7c456dfe935e36a
Svenstar74 <sven.firmbach@gmx.de>
2023-09-11 21:07:30 +0200

    use forwardRef

--- components/Screen/Screen.tsx
+++ components/Screen/Screen.tsx
@@ -1,28 +1,30 @@
+import { forwardRef, ReactNode } from "react";
 import { ScrollView, ScrollViewProps, View, ViewStyle } from "react-native";
+
 import Colors from "../../assets/colors";
 
 interface Props {
-  children: React.ReactNode;
+  children: ReactNode;
   style?: ViewStyle | ViewStyle[];
   props?: ScrollViewProps | ScrollViewProps[];
-  ref?: React.Ref<ScrollView>;
   view?: 'View' | 'ScrollView';
 }
 
-function Screen({ children, style = {} , props = {}, ref, view='ScrollView'}: Props) {
-  if (view === 'View') {
+const Screen = forwardRef<ScrollView, Props>(({ children, style = {}, props = {}, view = 'ScrollView' }, ref) => {
+    if (view === 'View') {
+      return (
+        <View style={{ flexGrow: 1, backgroundColor: Colors.themeBright, ...style }} {...props}>
+          {children}
+        </View>
+      );
+    }
+
     return (
-      <View style={{ flexGrow: 1, backgroundColor: Colors.themeBright, ...style }} {...props}>
+      <ScrollView contentContainerStyle={{ flexGrow: 1, backgroundColor: Colors.themeBright, ...style }} {...props} ref={ref}>
         {children}
-      </View>
+      </ScrollView>
     );
   }
-  
-  return (
-    <ScrollView contentContainerStyle={{ flexGrow: 1, backgroundColor: Colors.themeBright, ...style }} {...props} ref={ref}>
-      {children}
-    </ScrollView>
-  );
-}
+);
 
 export default Screen;
