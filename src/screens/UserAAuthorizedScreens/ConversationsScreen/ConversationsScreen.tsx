a762cf9077ec9b00a5bb7b1120674e59b138f851
Svenstar74 <sven.firmbach@gmx.de>
2023-10-23 20:39:44 +0200

    center text

--- src/screens/UserAAuthorizedScreens/ConversationsScreen/ConversationsScreen.tsx
+++ src/screens/UserAAuthorizedScreens/ConversationsScreen/ConversationsScreen.tsx
@@ -55,7 +55,7 @@ function ConversationsScreen() {
                 share it, so they can take the quiz, discover your shared values, and
                 pick topics to talk about.
               </CmTypography>
-              <CmTypography variant="caption">
+              <CmTypography variant="caption" style={{ textAlign: 'center' }}>
                 We will send you an email when they agree to share their results with you!
               </CmTypography>
               <CmTypography variant='label' style={styles.label}>Name of recipient</CmTypography>
