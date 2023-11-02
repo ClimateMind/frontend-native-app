b17c832eff7f94959ec1837e08a76df0668d1f80
Svenstar74 <sven.firmbach@gmx.de>
2023-11-02 20:33:38 +0100

    fix icon to startIcon

--- src/navigation/NavigationRootDrawer.tsx
+++ src/navigation/NavigationRootDrawer.tsx
@@ -38,11 +38,11 @@ function NavigationRootDrawer({ navigation }: Props) {
 
         {/* Login / Logout and Feedback button beneath the social images */}
         <View style={{ marginTop: 30 }}>
-          {!isLoggedIn && <CmButton text='LOG IN' icon={<MaterialIcons name="login" size={24} color="black" />} onPress={() => navigation.navigate('UserAUnauthorizedScreens', { screen: 'LoginScreen' })} />}
-          {isLoggedIn && <CmButton text='LOGOUT' icon={<MaterialIcons name="logout" size={24} color="black" />} onPress={onLogout} />}
+          {!isLoggedIn && <CmButton text='LOG IN' startIcon={<MaterialIcons name="login" size={24} color="black" />} onPress={() => navigation.navigate('UserAUnauthorizedScreens', { screen: 'LoginScreen' })} />}
+          {isLoggedIn && <CmButton text='LOGOUT' startIcon={<MaterialIcons name="logout" size={24} color="black" />} onPress={onLogout} />}
         </View>
         <View style={{ marginTop: 20 }}>
-          <CmButton text='FEEDBACK' icon={<MaterialIcons name="email" size={24} color="black" />} onPress={() => openUrl('mailto:hello@climatemind.org')} />
+          <CmButton text='FEEDBACK' startIcon={<MaterialIcons name="email" size={24} color="black" />} onPress={() => openUrl('mailto:hello@climatemind.org')} />
         </View>
       </View>
     </View>
