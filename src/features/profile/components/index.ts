d7fada8ef71406962366e009dfbf673db9755f06
epixieme <kirstie_hayes@hotmail.com>
2023-11-06 14:23:19 +0000

    migrated profile files

---
+++ src/features/profile/components/index.ts
@@ -0,0 +1,3 @@
+export { default as ChangePasswordModal } from './ChangePasswordModal';
+export { default as DeleteAccountModal } from './DeleteAccountModal';
+export { default as UpdateEmailModal } from './UpdateEmailModal';
