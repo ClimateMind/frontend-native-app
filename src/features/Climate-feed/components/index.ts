3ef026772e229c153dd77bf65ca312eb57f303bd
Sven Firmbach <sven.firmbach@gmx.de>
2023-11-07 14:30:09 +0100

    Delete src/features/Climate-feed directory

--- src/features/Climate-feed/components/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as ActionCardHeader } from './ActionCardHeader'
-export { default as ClimateFeedCard } from './ClimateFeedCard'
-export { default as ActionCard } from './ActionCard'
