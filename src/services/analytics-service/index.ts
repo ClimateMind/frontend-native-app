cfb74b86610eb3f3c566e30f2a50a31a510c2a88
Svenstar74 <sven.firmbach@gmx.de>
2023-11-23 10:39:02 +0100

    add analytics source

--- src/services/analytics-service/index.ts
+++ src/services/analytics-service/index.ts
@@ -1,3 +1,4 @@
+import { Platform } from 'react-native';
 import { AnalyticsService } from './AnalyticsService';
 
 const BASE_URL = process.env.EXPO_PUBLIC_API_URL;
@@ -6,4 +7,10 @@ if (!BASE_URL) {
 }
 
 export const analyticsService = new AnalyticsService(BASE_URL);
+if (Platform.OS === 'ios') {
+  analyticsService.setSource('mobile-ios');
+} else if (Platform.OS === 'android') {
+  analyticsService.setSource('mobile-android');
+}
+
 export * from './events';
