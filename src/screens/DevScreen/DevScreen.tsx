bca3e6cbe56b0a53e3dfdc1509a962a14ac20ff9
Svenstar74 <sven.firmbach@gmx.de>
2023-11-25 23:46:23 +0100

    update app in dev screen

--- src/screens/DevScreen/DevScreen.tsx
+++ src/screens/DevScreen/DevScreen.tsx
@@ -1,5 +1,9 @@
+import { StyleSheet, View } from 'react-native';
+
+import appConfig from '../../../app.json';
 import { CmButton, CmTypography, Content, Screen } from 'src/shared/components';
 import { useOnboarding } from 'src/features/onboarding/hooks';
+import { UpdateButton } from 'src/features/dev';
 
 function DevScreen() {
   const { resetOnboarding } = useOnboarding();
@@ -11,10 +15,23 @@ function DevScreen() {
           Dev Screen
         </CmTypography>
 
-        <CmButton onPress={resetOnboarding} text="Reset Onboarding" />
+        <CmButton onPress={resetOnboarding} text="Reset Onboarding" style={styles.btn} />
+
+        <UpdateButton />
+
+        <View style={{ flex: 1 }} />
+        <CmTypography variant='body' style={{ marginBottom: 20 }}>Version {appConfig.expo.version}</CmTypography>
       </Content>
     </Screen>
   );
 }
+
+const styles = StyleSheet.create({
+  btn: {
+    alignSelf: 'stretch',
+    marginHorizontal: 20,
+    marginBottom: 20,
+  },
+});
 
 export default DevScreen;
