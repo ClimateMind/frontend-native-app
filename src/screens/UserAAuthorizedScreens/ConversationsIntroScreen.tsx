3beda618f76d84faa9e56126c325a0c30fedbe4f
Svenstar74 <sven.firmbach@gmx.de>
2023-12-20 23:37:41 +0100

    reposition start talking button

--- src/screens/UserAAuthorizedScreens/ConversationsIntroScreen.tsx
+++ src/screens/UserAAuthorizedScreens/ConversationsIntroScreen.tsx
@@ -20,7 +20,10 @@ function ConversationsIntroScreen({ navigation }: Props) {
         <Content style={{ maxWidth: 400, alignItems: 'flex-start' }}>
           <CmTypography variant='h2' style={[styles.marginBottom, { alignSelf: 'center' }]}>How to talk about Climate Change</CmTypography>
 
-          <CmTypography variant='body' style={styles.marginBottom}>Climate change is a global issue, and your contribution matters!</CmTypography>
+          <CmTypography variant='body'>Climate change is a global issue, and your contribution matters!</CmTypography>
+
+          <CmButton style={styles.button} text='START TALKING WITH PEOPLE' onPress={navigateToConversationsScreen} />
+
           <CmTypography variant='body' style={styles.marginBottom}>You don't need to be an expert to talk about climate change. Follow these easy steps to start having effective conversations:</CmTypography>
           
           <BulletListItem>Send a friend the link we provide</BulletListItem>
@@ -29,8 +32,6 @@ function ConversationsIntroScreen({ navigation }: Props) {
           <BulletListItem textStyle={styles.marginBottom}>Tell us how it went!</BulletListItem>
 
           <CmTypography variant='body' style={styles.marginBottom}>When you have conversations with others, you take part in a network of people around the world who are working together to find solutions.</CmTypography>
-
-          <CmButton style={styles.button} text='START TALKING WITH PEOPLE' onPress={navigateToConversationsScreen} />
         </Content>
       </Section>
     </Screen>
@@ -43,7 +44,7 @@ const styles = StyleSheet.create({
   },
   button: {
     marginTop: 30,
-    marginBottom: 100,
+    marginBottom: 30,
     alignSelf: 'center',
   },
 });
