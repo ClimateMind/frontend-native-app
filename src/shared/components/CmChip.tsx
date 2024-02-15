77aa94620917918fc01f84f5de6d1ad99e668943
Kirstie <39728053+epixieme@users.noreply.github.com>
2024-02-15 14:38:32 +0000

    Improved Caret

--- src/shared/components/CmChip.tsx
+++ src/shared/components/CmChip.tsx
@@ -8,38 +8,37 @@ interface Props {
   personalValueText: {};
 }
 
-
 // get real descriptions for types
 const personalValueText = {
-  benevolence: ' Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam ea obcaecati distinctio reiciendis debitis autem cumque modi ratione doloremque corrupti, perspiciatis molestias excepturi, dolorum reprehenderit pariatur at voluptatum ipsum laborum.',
-  hedonism: ' Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam ea obcaecati distinctio reiciendis debitis autem cumque modi ratione doloremque corrupti, perspiciatis molestias excepturi, dolorum reprehenderit pariatur at voluptatum ipsum laborum.',
-  security: ' Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam ea obcaecati distinctio reiciendis debitis autem cumque modi ratione doloremque corrupti, perspiciatis molestias excepturi, dolorum reprehenderit pariatur at voluptatum ipsum laborum.',
-  tradition: ' Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam ea obcaecati distinctio reiciendis debitis autem cumque modi ratione doloremque corrupti, perspiciatis molestias excepturi, dolorum reprehenderit pariatur at voluptatum ipsum laborum.',
-  universalism: ' Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam ea obcaecati distinctio reiciendis debitis autem cumque modi ratione doloremque corrupti, perspiciatis molestias excepturi, dolorum reprehenderit pariatur at voluptatum ipsum laborum.',
-  security: ' Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam ea obcaecati distinctio reiciendis debitis autem cumque modi ratione doloremque corrupti, perspiciatis molestias excepturi, dolorum reprehenderit pariatur at voluptatum ipsum laborum.',
-
-
+  benevolence: "It's very important to them to help the people around them. They want to care for the well-being of those around them.",
+  hedonism: 'They seek every chance they can to have fun. It is important to them to do things that give them pleasure.',
+  security: 'It is important to them that things be organized and clean. They really do not like things to be a mess.',
+  tradition: 'Religious belief or traditions are important to them. They try hard to do what their religion or family traditions require.',
+  universalism: 'They think it is important that every person in the world be treated equally. They believe everyone should have equal opportunities in life.',
+  'self-direction': "They think it's important to be interested in things. They like to be curious and to try to understand all sorts of things.",
+  conformity: 'They believe they should always show respect to their parents and to older people. It is important to them to be obedient.',
+  stimulation: 'They like to take risks. They are always looking for adventures.',
+  achievement: 'Being very successful is important to them. They like to impress other people.',
+  power: 'It is important to them to be in charge and tell others what to do. They want people to do what they say.',
 };
 
 function CmChip({ label }: Props) {
   const [tooltip, setTooltip] = useState(false);
 
   return (
-  
     <View style={{ position: 'relative' }}>
       {tooltip && (
         <View style={styles.tooltip}>
-           <CmTypography variant="h1" style={styles.tooltipText}>
+          {/* <CmTypography variant="h1" style={styles.tooltipText}>
             {label}
-          </CmTypography>
+          </CmTypography> */}
           <CmTypography variant="body" style={styles.tooltipText}>
             {personalValueText[label]}
           </CmTypography>
           <View style={styles.caretDown}></View>
         </View>
       )}
-        
-    
+
       <Pressable onTouchStart={() => setTooltip(true)} onTouchEnd={() => setTooltip(false)}>
         <CmTypography variant="body" style={styles.chip}>
           {label}
@@ -58,6 +57,8 @@ const styles = StyleSheet.create({
     margin: 5,
   },
   tooltip: {
+    width: 100,
+    height: 200,
     position: 'absolute',
     backgroundColor: 'rgba(255, 255, 255, 1)',
     padding: 8,
@@ -79,23 +80,22 @@ const styles = StyleSheet.create({
     zIndex: 10000,
   },
   tooltipText: {
+    justifyContent: 'center',
     color: 'black',
     fontSize: 14,
   },
   caretDown: {
-    position:'absolute',
-   backgroundColor: 'white',
-   borderWidth:1,
-   width:20,
-   height:20,
-   transform: [{ rotate: '45deg'}],
-   bottom:'-3%',
-   left: '40%',
-   borderTopWidth:0,
-   borderLeftWidth:0
-   
-
-   
+    backgroundColor: 'white',
+    borderRadius: 2,
+    borderWidth: 1,
+    width: 20,
+    height: 20,
+    transform: [{ rotate: '45deg' }],
+    borderTopWidth: 0,
+    borderLeftWidth: 0,
+    position: 'absolute',
+    bottom: '-6%',
+    left: '50%',
   },
 });
 
